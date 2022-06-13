dafe7df8a9e4adc71c27d6ff190158de450eb2a5
SophiaDuboc <sophiapaula4@gmail.com>
2022-06-12 21:26:22 -0300

    inicializando sequelize e conexao com mongo

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoClient = require("mongodb").MongoClient;
+mongoClient.connect("mongodb+srv://admin:admin@encurtadorurl.iu8f1.mongodb.net/?retryWrites=true&w=majority")
+            .then(conn => global.conn = conn.db("encurtador"))
+            .catch(err => console.log(err))
+
+function findAll() {
+    return global.conn.collection("urls").find().toArray();
+}
+ 
+module.exports = { findAll }
