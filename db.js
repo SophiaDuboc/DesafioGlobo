91536892d5c281c1a808b5cbaac82833bfc3667a
SophiaDuboc <sophiapaula4@gmail.com>
2022-06-14 01:00:43 -0300

    arrumando a casa

--- db.js
+++ db.js
@@ -1,37 +1,40 @@
 const mongoClient = require("mongodb").MongoClient;
 mongoClient.connect("mongodb+srv://admin:admin@encurtadorurl.iu8f1.mongodb.net/?retryWrites=true&w=majority")
             .then(conn => global.conn = conn.db("encurtador"))
-            .catch(err => console.log(err))
+            .catch(err => console.log(err));
 
 function findAll() {
     return global.conn.collection("urls").find().toArray();
 }
 
-function insert(urlOriginal, urlCurta) {
-    return global.conn.collection("urls").insertOne({ "original":urlOriginal, "curta":urlCurta, "acessos": 0});
+function insert(urlOriginal, hash) {
+    return global.conn.collection("urls").insertOne({ "original":urlOriginal, "hash":hash, "acessos": 0});
 }
 
-function findOne(url){
-    let search = getSearch(url);
+function findOne(find){
+    let search = getSearch(find);
     return (global.conn.collection("urls").find(search).toArray());
 }
 
 function updateAcessos(encurtador){
-    qtdAcessos = encurtador.acessos + 1
-    return global.conn.collection("urls").updateOne({"original": encurtador.original}, {$set:{"acessos": qtdAcessos}})
+    qtdAcessos = encurtador.acessos + 1;
+    return global.conn.collection("urls").updateOne({"original": encurtador.original}, {$set:{"acessos": qtdAcessos}});
 }
 
-function deleteOne(url){
-    global.conn.collection("urls").deleteOne({"original": url })
+function deleteOne(find){
+    let search = getSearch(find);
+    global.conn.collection("urls").deleteOne(search);
 }
 
-function getSearch(url){
-    if(url.includes("localhost")){
-        var search = {"curta": url } 
+/* /* /* /* /* /* /* /* /* /* /* /* /* FUNÇÕES AUXILIARES /* /* /* /* /* /* /* /* /* /* /* */
+function getSearch(find){
+    if(find.includes("http")){
+        var search = {"original": find } 
     }else{
-        var search = {"original": url }
+        var search = {"hash": find }
     }
     return search;
 }
+
 
 module.exports = { findAll , insert , findOne , updateAcessos , deleteOne}
---
+++ src/geradorDeHash.js
@@ -0,0 +1,21 @@
+
+
+ function geraHash() {
+    let tamanhoHash = tamanhoAletorio();
+    return geraHashAleatoria(tamanhoHash)
+  }
+  
+  function geraHashAleatoria(tamanhoHash) {
+    let hash = '';
+    let caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < tamanhoHash; i++) {
+      hash += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+    }
+    return hash;
+  }
+  
+  function tamanhoAletorio() {
+    const tamanhoMinimoHash = 5
+    const tamanhoMaximoHash = 7
+    return Math.floor(Math.random() * (tamanhoMaximoHash - tamanhoMinimoHash + 1)) + tamanhoMinimoHash;
+  }
